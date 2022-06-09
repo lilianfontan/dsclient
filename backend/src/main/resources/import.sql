4b65f3f006970329bf7bc98986088355f68aa86c
lilianfontan <lilian.fontan@gmail.com>
2022-06-08 21:53:19 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,17 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Maria Aparecida', '123456789-00', 1000.00, TIMESTAMP WITH TIME ZONE '1984-05-10T00:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('João Paulo', '234567890-01', 1500.00, TIMESTAMP WITH TIME ZONE '1985-06-15T00:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Priscila Souza', '345678901-02', 2000.00, TIMESTAMP WITH TIME ZONE '1986-07-18T00:00:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Cristiano Silva', '456789123-04', 2500.00, TIMESTAMP WITH TIME ZONE '1983-01-02T00:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Paula Bueno', '987654321-22', 3000.00, TIMESTAMP WITH TIME ZONE '1980-10-02T00:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Joaquim Oliveira', '852963741-65', 3500.00, TIMESTAMP WITH TIME ZONE '1981-08-04T00:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Joana Darc', '951623847-98', 4000.00, TIMESTAMP WITH TIME ZONE '1975-04-03T00:00:00Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Henrique VIII', '741963852-63', 4500.00, TIMESTAMP WITH TIME ZONE '1984-05-08T00:00:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ana Bolena', '654789123-10', 5000.00, TIMESTAMP WITH TIME ZONE '1980-03-25T00:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jack Brigance', '65237458-74', 5500.00, TIMESTAMP WITH TIME ZONE '1977-06-28T00:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Catarina de Aragão', '856952367-45', 6000.00, TIMESTAMP WITH TIME ZONE '1974-08-01T00:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Harry Potter', '758415632-97', 6500.00, TIMESTAMP WITH TIME ZONE '1980-07-11T00:00:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Hermione Granger', '695325147-15', 7000.00, TIMESTAMP WITH TIME ZONE '1980-03-10T00:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Rony Wesley', '125325859-41', 7500.00, TIMESTAMP WITH TIME ZONE '1980-05-14T00:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Meredith Grey', '426846963-74', 8000.00, TIMESTAMP WITH TIME ZONE '1984-10-30T00:00:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Albus Dumbledore', '458745963-00', 8500.00, TIMESTAMP WITH TIME ZONE '1960-10-31T00:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Cristina Yang', '953842751-80', 9000.00, TIMESTAMP WITH TIME ZONE '1984-01-05T00:00:00Z', 0);
